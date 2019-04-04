811f9c68dd393bbdb54565463a78aa23689b2f0c
Prameelarani Vidyasagar Sagili <prameelarani.sa@itp.objectfrontier.com>
2019-04-04 16:12:43 +0530

    added homepage

---
+++ RestAssuredLearning/src/homePage.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+ 
+public class SimpleGetTest {
+}
